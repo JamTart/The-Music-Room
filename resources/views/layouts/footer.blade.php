a89d1127e5becc26713d0a746f454f27a555589a
JamTart <jamalwinchester@gmail.com>
2018-07-28 11:32:00 -0400

    Some updates made to the login page + footer started

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+<head>
+  <style>
+    footer{
+      background-color:#75CDD1;
+      bottom: 0;
+      position: absolute;
+      width: 100%;
+    }
+  </style>
+</head>
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="#"> themusicroom.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
